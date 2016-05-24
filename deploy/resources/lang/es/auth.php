cfab8c3c73365519aba62911045803015c951734
Stivemass <hernan.pineros@mass.com.co>
2016-05-24 09:13:14 -0500

    Se agrego la carpeta "ES", path español.

--- deploy/resources/lang/es/auth.php
+++ deploy/resources/lang/es/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'Estas credenciales no coinciden con nuestros registros.',
-    'throttle' => 'Demasiados intentos de inicio de sesión. Vuelva a intentarlo en :seconds segundos.',
+    'failed' => 'Estas credenciales no coinciden con nuestros registros. ',
+    'throttle' => 'Demasiados intentos de inicio de sesión. Vuelva a intentarlo en :seconds segundos. ',
 
 ];
--- deploy/resources/lang/es/passwords.php
+++ deploy/resources/lang/es/passwords.php
@@ -10,7 +10,7 @@ return [
     | The following language lines are the default lines which match reasons
     | that are given by the password broker for a password update attempt
     | has failed, such as for an invalid token or invalid new password.
-    |
+    | 
     */
 
     'password' => 'La contraseña debe tener al menos 6 caracteres y coincidir con la confirmación.',
